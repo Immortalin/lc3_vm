62b6b2e5229f56d61a5290b2edd5d56992a2a456
Immortalin <7126128+Immortalin@users.noreply.github.com>
2022-01-02 17:59:37 -0500

    More idomatic rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use lazy_static::lazy_static;
+use std::rc::Rc;
 use std::sync::Mutex;
 
-static PC_START: u16 = 0x3000;
+const PC_START: u16 = 0x3000;
 const NOPS: usize = 16;
 
 enum Register {
@@ -17,56 +17,103 @@ enum Register {
     RCND,
     RCNT,
 }
+enum Flag {
+    FP = 1 << 0,
+    FZ = 1 << 1,
+    FN = 1 << 2,
+}
 
-/// Array of opcodes to be executed based on their index
-// v_str refers to vm str as `str` is a reserved word in rust
-const op_ex: [fn(u16); NOPS] = [
-    br, add, ld, st, jsr, and, ldr, v_str, rti, not, ldi, sti, jmp, res, lea, trap,
-];
-
-lazy_static! {
-    static ref mem: Mutex::<[u16; u16::MAX as usize]> = Mutex::new([0; u16::MAX as usize]);
-    static ref regs: Mutex::<[u16; Register::RCNT as usize]> =
-        Mutex::new([0; Register::RCNT as usize]);
+struct VM {
+    // pc: u16,
+    regs: [u16; Register::RCNT as usize],
+    mem: [u16; u16::MAX as usize],
+    /// Op code execution lookup table,
+    /// array of opcodes to be executed based on their index.
+    /// `v_str` refers to vm str as `str` is a reserved word in rust
+    op_ex: [fn(&VM, u16); NOPS],
 }
 
 fn main() {
-    mw(0, 0x1234);
-    op_ex[0](0);
-    println!("Location at address {} is {}", 0, mem.lock().unwrap()[0]);
+    let mut vm = VM::new();
+    vm.mw(0, 1234);
+    vm.do_op(0, 0);
+    println!("Location at address {} is {}", 0, vm.mem[0]);
 }
 
-fn br(i: u16) {
-    println!("br");
-}
-fn add(i: u16) {}
-fn ld(i: u16) {}
-fn st(i: u16) {}
-fn jsr(i: u16) {}
-fn and(i: u16) {}
-fn ldr(i: u16) {}
-fn v_str(i: u16) {}
-fn rti(i: u16) {}
-fn not(i: u16) {}
-fn ldi(i: u16) {}
-fn sti(i: u16) {}
-fn jmp(i: u16) {}
-fn res(i: u16) {}
-fn lea(i: u16) {}
-fn trap(i: u16) {}
+impl VM {
+    fn new() -> Self {
+        Self {
+            regs: [0; Register::RCNT as usize],
+            mem: [0; u16::MAX as usize],
+            op_ex: [
+                Self::br,
+                Self::add,
+                Self::ld,
+                Self::st,
+                Self::jsr,
+                Self::and,
+                Self::ldr,
+                Self::v_str,
+                Self::rti,
+                Self::not,
+                Self::ldi,
+                Self::sti,
+                Self::jmp,
+                Self::res,
+                Self::lea,
+                Self::trap,
+            ],
+        }
+    }
 
-/// Shifts the instruction by 12 to get the leading 4 bits which is the opcode.
-#[inline(always)]
-fn opc(i: u16) -> u16 {
-    i >> 12
-}
+    fn do_op(&self, op: usize, i: u16) {
+        self.op_ex[op](self, i);
+    }
 
-#[inline(always)]
-fn mr(address: u16) -> u16 {
-    mem.lock().unwrap()[address as usize]
-}
+    fn br(&self, i: u16) {
+        println!("br");
+    }
+    fn add(&self, i: u16) {}
+    fn ld(&self, i: u16) {}
+    fn st(&self, i: u16) {}
+    fn jsr(&self, i: u16) {}
+    fn and(&self, i: u16) {}
+    fn ldr(&self, i: u16) {}
+    fn v_str(&self, i: u16) {}
+    fn rti(&self, i: u16) {}
+    fn not(&self, i: u16) {}
+    fn ldi(&self, i: u16) {}
+    fn sti(&self, i: u16) {}
+    fn jmp(&self, i: u16) {}
+    fn res(&self, i: u16) {}
+    fn lea(&self, i: u16) {}
+    fn trap(&self, i: u16) {}
 
-#[inline(always)]
-fn mw(address: u16, value: u16) {
-    mem.lock().unwrap()[address as usize] = value;
+    #[inline(always)]
+    fn process_signed_num(&mut self, reg: Register) {
+        let r = reg as usize;
+        if self.regs[r] == 0 {
+            self.regs[Register::RCND as usize] = Flag::FZ as u16; // the value in r is zero
+        } else if self.regs[r] >> 15 != 0 {
+            self.regs[Register::RCND as usize] = Flag::FN as u16; // the value in r is z negative number
+        } else {
+            self.regs[Register::RCND as usize] = Flag::FP as u16; // the value in r is a positive number
+        }
+    }
+
+    /// Shifts the instruction by 12 to get the leading 4 bits which is the opcode.
+    #[inline(always)]
+    fn opc(i: u16) -> u16 {
+        i >> 12
+    }
+
+    #[inline(always)]
+    fn mr(&self, address: u16) -> u16 {
+        self.mem[address as usize]
+    }
+
+    #[inline(always)]
+    fn mw(&mut self, address: u16, value: u16) {
+        self.mem[address as usize] = value;
+    }
 }
